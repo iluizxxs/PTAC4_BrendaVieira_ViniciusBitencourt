370474740bf98c137306cff1cbb030301c2bfdbf
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-11-25 23:19:01 -0400

    ccmmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
+import styles from "../app/css/home.module.css"
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { setCookie } from 'nookies';
 import { parseCookies } from 'nookies';
 
 export default function Home(){
@@ -16,28 +16,8 @@ const router = useRouter();
 
   return (
     <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">E-mail:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">Senha:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} /> 
-        </div>
-        <button type="submit">Login</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </form>
-      <a href='/Cadastro'>Se Cadastre</a>
+      <h1>HOME</h1>
+      
     </div>
   );
 };
